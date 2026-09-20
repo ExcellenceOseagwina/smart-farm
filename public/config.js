53a4f7ed1b08f39c83643be213944d97558977bf
Excellence Oseagwina <oseagwinaexcellence@gmail.com>
2026-09-20 18:19:22 +0100

    Added favicons and removed unnecessary comments🍀

--- public/config.js
+++ public/config.js
@@ -1,11 +1,8 @@
-// Replace with your keys
 const SB_URL = "https://nuqbymedtideqtrcnljg.supabase.co";
 const SB_KEY = "sb_publishable_WkMCQzXhucA4u5w9kqUVJA_BAMucHqj";
 
-// Initialize Supabase once
 const _supabase = supabase.createClient(SB_URL, SB_KEY);
 
-// Helper to check if user is logged in
 async function checkUser() {
     const { data: { user } } = await _supabase.auth.getUser();
     if (!user && !window.location.href.includes('auth.html')) {
@@ -14,7 +11,6 @@ async function checkUser() {
     return user;
 }
 
-// Global Logout function
 async function logout() {
     await _supabase.auth.signOut();
     localStorage.clear();
